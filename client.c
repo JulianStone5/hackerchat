b2cdeff845f8ea418bb7badebfb13284ea3be9ed
xieruishen <xierui.shen@students.olin.edu>
2019-03-09 11:59:45 -0500

    add pthread function to client

--- client.c
+++ client.c
@@ -1,16 +1,34 @@
 // Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 #define PORT 4444
 #define BUF_SIZE 1024
+
+void * receiveMessage(void * socket) {
+ int sockfd, ret;
+ char buffer[BUF_SIZE];
+ sockfd = (int) socket;
+ memset(buffer, 0, BUF_SIZE);
+ for (;;) {
+  ret = read(sockfd , buffer, BUF_SIZE);
+  if (ret < 0) {
+   printf("Error receiving data!\n");
+  } else {
+   printf("server: ");
+   fputs(buffer, stdout);
+   //printf("\n");
+  }
+ }
+}
 
 int main(int argc, char const *argv[])
 {
@@ -20,6 +38,7 @@ int main(int argc, char const *argv[])
     char *hello = "Hello from client";
     char buffer[BUF_SIZE] = {0};
     char * serverAddr;
+    pthread_t rThread;
 
     if (argc <2) { //checks that user input server ip address
       printf("Missing server ip address");
@@ -53,21 +72,24 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
+    //creating a new thread for receiving messages from the server
+    int ret = pthread_create(&rThread, NULL, receiveMessage, (void *) sock);
+    if (ret) {
+     printf("ERROR: Return Code from pthread_create() is %d\n", ret);
+     exit(1);
+    }
+
     while(fgets(buffer, BUF_SIZE, stdin) != NULL) {
         //int ret = send(sock , hello , strlen(hello) , 0 );
-        int ret = send(sock , buffer , BUF_SIZE , 0 );
+        ret = send(sock , buffer , BUF_SIZE , 0);
         if(ret < 0) {
           printf("Error sending data");
         }
         printf("Message sent\n");
-        //creating a new thread for receiving messages from the server
-        ret = pthread_create(&rThread, NULL, read, (void *) sockfd);
-        if (ret) {
-         printf("ERROR: Return Code from pthread_create() is %d\n", ret);
-         exit(1);
-        }
         //valread = read( sock , buffer, 1024);
-        printf("%s\n",buffer );
+        //printf("%s\n",buffer );
     }
+    close(sock);
+    pthread_exit(NULL);
     return 0;
 }
